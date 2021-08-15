fbea90ee40cf02f579207956e8d78553068a9f2a
unknown <66fatihavci@gmail.com>
2021-08-15 15:53:20 +0300

    ikinci yüklemem

--- app/Http/Controllers/Front/Homepage.php
+++ app/Http/Controllers/Front/Homepage.php
@@ -44,9 +44,9 @@ class Homepage extends Controller
     }
 
     public function category($slug){
-            $cat=Category::where('slug',$slug)->where('status',1)->first() ?? abort(403,'Böyle bir data bulunamadı');
+            $cat2=Category::where('slug',$slug)->where('status',1)->first() ?? abort(403,'Böyle bir data bulunamadı');
            // $data['categories']=Category::orderBy('id', 'asc')->get();//__consturcta share ettiğimiz için gerek kalmadı
-            $data['kategoridetay']=$cat;
+            $data['kategoridetay']=$cat2;
             $data['icerik']=content::where('category_id',$cat->id)->where('status',1)->paginate(1);
            
             return view('front.categorydetay',$data);
