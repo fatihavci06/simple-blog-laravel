6631e1c841ce7a5eb7610c5b0caf3022c772ee6d
unknown <66fatihavci@gmail.com>
2021-08-15 16:04:17 +0300

    4

--- app/Http/Controllers/Front/Homepage.php
+++ app/Http/Controllers/Front/Homepage.php
@@ -44,10 +44,10 @@ class Homepage extends Controller
     }
 
     public function category($slug){
-            $cat=Category::where('slug',$slug)->where('status',1)->first() ?? abort(403,'Böyle bir data bulunamadı');
+            $cat2=Category::where('slug',$slug)->where('status',1)->first() ?? abort(403,'Böyle bir data bulunamadı');
            // $data['categories']=Category::orderBy('id', 'asc')->get();//__consturcta share ettiğimiz için gerek kalmadı
-            $data['kategoridetay']=$cat;
-            $data['icerik']=content::where('category_id',$cat->id)->where('status',1)->paginate(1);
+            $data['kategoridetay']=$cat2;
+            $data['icerik']=content::where('category_id',$cat2->id)->where('status',1)->paginate(1);
            
             return view('front.categorydetay',$data);
     }
